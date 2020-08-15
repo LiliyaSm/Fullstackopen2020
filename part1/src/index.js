1584920f80ea16d19d33ff81799caedb5da63669
Liliia Smelova <liliia.smelova@outlook.com>
2020-08-15 18:16:23 +0300

    1.4: course information step4

--- part1/src/index.js
+++ part1/src/index.js
@@ -33,9 +33,18 @@ const App = () => {
     const Content = (props) => {
         return (
             <div>
-                <Part part={parts[0].name} exercises={parts[0].exercises} />
-                <Part part={parts[1].name} exercises={parts[1].exercises} />
-                <Part part={parts[2].name} exercises={parts[2].exercises} />
+                <Part
+                    part={props.parts[0].name}
+                    exercises={props.parts[0].exercises}
+                />
+                <Part
+                    part={props.parts[1].name}
+                    exercises={props.parts[1].exercises}
+                />
+                <Part
+                    part={props.parts[2].name}
+                    exercises={props.parts[2].exercises}
+                />
             </div>
         );
     };
@@ -44,7 +53,9 @@ const App = () => {
         return (
             <p>
                 Number of exercises{" "}
-                {props.exercises1 + props.exercises2 + props.exercises3}
+                {props.parts[0].exercises +
+                    props.parts[1].exercises +
+                    props.parts[2].exercises}
             </p>
         );
     };
@@ -52,12 +63,8 @@ const App = () => {
     return (
         <div>
             <Header course={course} />
-            <Content />
-            <Total
-                exercises1={parts[0].exercises}
-                exercises2={parts[1].exercises}
-                exercises3={parts[2].exercises}
-            />
+            <Content parts={parts} />
+            <Total parts={parts} />
         </div>
     );
 };
