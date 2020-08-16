b1510ebf734504285b5402fc0fa053af85efdf05
Liliia Smelova <liliia.smelova@outlook.com>
2020-08-16 18:04:28 +0300

    1.7: unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-
 const Header = (props) => {
     return <h1>{props.text}</h1>;
 };
@@ -17,27 +16,45 @@ const Display = (props) => (
     </div>
 );
 
-const App = () => {
+const Average = (props) => {
+    let average = (props.good * 1 - props.bad * 1) / props.all;
 
+    if (isNaN(average)) {
+        average = 0;
+    }
+    return <div>average {average} </div>;
+};
+const Positive = (props) => {
+    let positive = (100 * props.good) / props.all;
+    if (isNaN(positive)) {
+        positive = 0;
+    }
+    return <div>positive {positive} %</div>;
+};
+const App = () => {
     // save clicks of each button to own state
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+    const all = good + neutral + bad;
 
     return (
         <div>
             {" "}
             <Header text="Give feedback" />
-            <Button handleClick={() => setGood(good + 1)} text="Good" />
+            <Button handleClick={() => setGood(good + 1)} text="good" />
             <Button
                 handleClick={() => setNeutral(neutral + 1)}
-                text="Neutral"
+                text="neutral"
             />
-            <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+            <Button handleClick={() => setBad(bad + 1)} text="bad" />
             <Header text="Statistics" />
-            <Display value={good} text="Good " />
-            <Display value={neutral} text="Neutral " />
-            <Display value={bad} text="Bad " />
+            <Display value={good} text="good " />
+            <Display value={neutral} text="neutral " />
+            <Display value={bad} text="bad " />
+            <Display value={all} text="all " />
+            <Average good={good} bad={bad} all={all} />
+            <Positive good={good} all={all} />
         </div>
     );
 };
