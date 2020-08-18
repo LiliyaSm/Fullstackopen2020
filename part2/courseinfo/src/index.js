8d71a474d361243f963d30bc378539c36d9366b7
Liliia Smelova <liliia.smelova@outlook.com>
2020-08-18 13:41:53 +0300

    2.4: Course information step9

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Header = ({ course }) => {
-    return <h1>{course.name}</h1>;
+    return <h2>{course.name}</h2>;
 };
 
 const Total = ({ parts }) => {
     const sum = parts.reduce((acc, x) => acc + x.exercises, 0);
-    return <p>total of {sum} exercises </p>;
+    return <p><strong>total of {sum} exercises</strong> </p>;
 };
 
 const Part = (props) => {
@@ -38,32 +38,62 @@ const Course = ({ course }) => {
     );
 };
 
-
 const App = () => {
-    const course = {
-        id: 1,
-        name: "Half Stack application development",
-        parts: [
-            {
-                name: "Fundamentals of React",
-                exercises: 10,
-                id: 1,
-            },
-            {
-                name: "Using props to pass data",
-                exercises: 7,
-                id: 2,
-            },
-            {
-                name: "State of a component",
-                exercises: 14,
-                id: 3,
-            },
-        ],
-    };
+    const courses = [
+        {
+            name: "Half Stack application development",
+            id: 1,
+            parts: [
+                {
+                    name: "Fundamentals of React",
+                    exercises: 10,
+                    id: 1,
+                },
+                {
+                    name: "Using props to pass data",
+                    exercises: 7,
+                    id: 2,
+                },
+                {
+                    name: "State of a component",
+                    exercises: 14,
+                    id: 3,
+                },
+                {
+                    name: "Redux",
+                    exercises: 11,
+                    id: 4,
+                },
+            ],
+        },
+        {
+            name: "Node.js",
+            id: 2,
+            parts: [
+                {
+                    name: "Routing",
+                    exercises: 3,
+                    id: 1,
+                },
+                {
+                    name: "Middlewares",
+                    exercises: 7,
+                    id: 2,
+                },
+            ],
+        },
+    ];
 
-    return <Course course={course} />;
+    return (
+        <div>
+            <h1>Web development curriculum</h1>
+            <div>
+                {courses.map((course) => (
+                    <Course key={course.id} course={course} />
+                ))}
+            </div>
+        </div>
+    );
 };
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
