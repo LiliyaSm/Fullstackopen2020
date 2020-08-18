67690a6744397c57ad5f37905787c3aa71b1e7f2
Liliia Smelova <liliia.smelova@outlook.com>
2020-08-18 21:14:13 +0300

    2.9*: The Phonebook Step4

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -9,9 +9,17 @@ const Person = ({ person }) => {
 };
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", number: "040-123456" },
+        { name: "Ada Lovelace", number: "39-44-5323523" },
+        { name: "Dan Abramov", number: "12-43-234345" },
+        { name: "Mary Poppendieck", number: "39-23-6423122" },
+    ]);
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
+    const [filter, setFilter] = useState("");
+
+    const personsToShow =  persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()));
 
     const addName = (event) => {
         event.preventDefault();
@@ -20,7 +28,7 @@ const App = () => {
             number: newNumber,
         };
         // "some" tests whether at least one element in the array passes the test implemented by the provided function
-        if (persons.some((el) => el.name === nameObject.name)) {
+        if (persons.some((person) => person.name === nameObject.name)) {
             alert(`${nameObject.name} is already added to phonebook`);
         } else {
             setPersons(persons.concat(nameObject));
@@ -39,9 +47,19 @@ const App = () => {
         setNewNumber(event.target.value);
     };
 
+    const handleFilter = (event) => {
+        setFilter(event.target.value);
+    };
+
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                {/* filtering logic is case insensitive */}
+                filter shown with: <input value={filter} onChange={handleFilter} />
+            </div>
+            <h2>Add a new</h2>
+
             <form onSubmit={addName}>
                 <div>
                     name: <input value={newName} onChange={handleNewName} />
@@ -56,7 +74,7 @@ const App = () => {
             </form>
             <h2>Numbers</h2>
             <div>
-                {persons.map((person) => (
+                {personsToShow.map((person) => (
                     <Person key={person.name} person={person} />
                 ))}
             </div>
